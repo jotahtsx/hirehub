3d5c6aed486273738be363c727b461102cc923c2
João Manoel <joaomanoelskai@gmail.com>
2025-10-17 08:26:35 -0300

    Inicializando projeto

---
+++ src/test/java/com/jotahdev/hire_hub/HireHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jotahdev.hire_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HireHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
