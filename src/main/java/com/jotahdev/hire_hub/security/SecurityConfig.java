de1dc0864638e39ece500db7ad38963493ff2a38
João Manoel <joaomanoelskai@gmail.com>
2025-10-22 08:44:13 -0300

    Criando rotas públicas

--- src/main/java/com/jotahdev/hire_hub/security/SecurityConfig.java
+++ src/main/java/com/jotahdev/hire_hub/security/SecurityConfig.java
@@ -7,11 +7,17 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-    
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable());
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/candidato/**").permitAll()
+                .requestMatchers("/empresa/**").permitAll()
+                .anyRequest().authenticated()
+            );
+
         return http.build();
     }
-
 }
