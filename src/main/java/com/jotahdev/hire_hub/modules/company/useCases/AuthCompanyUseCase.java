2949a179915bc6a393dd1421a0158f26dfd20463
João Manoel <joaomanoelskai@gmail.com>
2025-10-23 09:15:26 -0300

    Iniciando Autenticação e Autorização

--- src/main/java/com/jotahdev/hire_hub/modules/company/useCases/AuthCompanyUseCase.java
+++ src/main/java/com/jotahdev/hire_hub/modules/company/useCases/AuthCompanyUseCase.java
@@ -1,8 +1,34 @@
 package com.jotahdev.hire_hub.modules.company.useCases;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import com.jotahdev.hire_hub.modules.company.dto.AuthCompanyDTO;
+import com.jotahdev.hire_hub.modules.company.repositories.CompanyRepository;
 
 @Service
 public class AuthCompanyUseCase {
-    
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public void execute(AuthCompanyDTO authCompanyDTO) {
+
+        var company = this.companyRepository.findByUsername(authCompanyDTO.getUsername())
+            .orElseThrow(() -> new UsernameNotFoundException("A empresa solicitada não foi encontrada."));
+
+        var passwordMatches = this.passwordEncoder.matches(authCompanyDTO.getPassword(), company.getPassword());
+
+        if (!passwordMatches) {
+            throw new BadCredentialsException("A senha informada está incorreta. Tente novamente.");
+        }
+
+        // Aqui você pode gerar o JWT ou retornar os dados da empresa autenticada
+    }
 }
